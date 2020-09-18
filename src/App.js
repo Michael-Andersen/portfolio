9c1283d73087e90f1ca220a4be327108129005cc
Michael Andersen <mandersen22@my.bcit.ca>
2020-09-18 17:17:29 -0400

    fixed hamburger

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import Container from 'react-bootstrap/Container'
 import Col from 'react-bootstrap/Col'
 import Row from 'react-bootstrap/Row'
 import Image from 'react-bootstrap/Image';
+import ListGroup from 'react-bootstrap/ListGroup'
+import ListGroupItem from 'react-bootstrap/ListGroupItem'
 
 import About from './about.js'
 import Education from './education.js'
@@ -22,7 +24,68 @@ import hamburger from './hamburger.png'
 import './App.css';
 
 class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isHamburgerOpen: false
+    }
+  }
+
+  handleHamburgerClick() {
+    this.setState({isHamburgerOpen: !this.state.isHamburgerOpen});
+  }
+
+  renderHamburgerMenu(sections) {
+    const sectionDisplaysMobile = sections.map( (item) => {
+      return item.hasSubsection ? (
+        <NavLink className="text-decoration-none text-reset" to={item.path}><ListGroupItem key={item.title}>{item.title}</ListGroupItem></NavLink>
+          )
+          : (<NavLink className="text-decoration-none text-reset" exact to={item.path}><ListGroupItem className="list-group-item" key={item.title}>{item.title}</ListGroupItem></NavLink>
+          );
+      });
+  const menu = (<div>{this.state.isHamburgerOpen ? <ListGroup onClick={()=>this.handleHamburgerClick()} className="MobileList">{sectionDisplaysMobile}</ListGroup> : null}</div>);
+  return menu;
+}
+
+  renderHamburgerButton() {
+  return(<div className="HamburgerDiv"><Image src={hamburger} onClick= {() => this.handleHamburgerClick()} /></div>);
+  }
+
+  SidePanel(sections) {
+  const sectionDisplaysFull = sections.map((item) => {
+                  return item.hasSubsection ? (
+                    <li className="Section" key={item.title}><NavLink className="links text-decoration-none text-reset" to={item.path}>{item.title}</NavLink></li>
+                      )
+                      : (<li className="Section" key={item.title}><NavLink className="links text-decoration-none text-reset" exact to={item.path}>{item.title}</NavLink></li>
+                      );
+                });                
+                return (
+                  <div>
+                  <div className="SidePanel">
+                    <ul className="FullList">{sectionDisplaysFull}</ul></div>
+                  </div>);
+                  
+
+  }
+
+  renderContentSections() {
+    return (<div>
+    <Route exact path="/" component={About}/>
+        <Route path="/education" component={Education}/>
+        <Route path="/experience" component={Experience}/>
+        <Route exact path="/projects" component={Projects}/>
+        <Route path="/projects/imagemorpher" component={ImageMorpher}/></div>);
+  }
   render() {
+    const sections = [{title:"About", path: "/", hasSubsection: false}, 
+                {title:"Education", path: "/education", hasSubsection: false}, 
+                {title:"Experience", path: "/experience", hasSubsection: false}, 
+                {title:"Projects", path:"/projects", hasSubsection: true}]
+    const sidePanel = this.SidePanel(sections);
+    const hamButton = this.renderHamburgerButton();
+    const hamMenu = this.renderHamburgerMenu(sections);
+    const contentSections = this.renderContentSections();
     return (
       <HashRouter>
       <Container fluid className="TopView">
@@ -30,81 +93,20 @@ class App extends React.Component {
       <Col className="App-header">
           Michael Andersen
       </Col><Col className="SideCol">
-      <SidePanel/>
+      {sidePanel}
+      {hamButton}
       </Col>
       </Row>
       </Container>
       <div className="MainView">
-        <Route exact path="/" component={About}/>
-        <Route path="/education" component={Education}/>
-        <Route path="/experience" component={Experience}/>
-        <Route exact path="/projects" component={Projects}/>
-        <Route path="/projects/imagemorpher" component={ImageMorpher}/>
+        {this.state.isHamburgerOpen? hamMenu : contentSections}
+        
       </div>
       
       </HashRouter>
     );  
   }
 }
-
-function DropdownButton(props) {
-  return (
-    <option value={props.path} key={props.title}>
-      {props.title}
-    </option>
-  );
-}
-
-class HamburgerMenu extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isOpen: false
-    }
-  }
-  handleClick() {
-    this.setState({isOpen: !this.state.isOpen});
-  }
-
-  render(){
-  const menu = (<div>{this.state.isOpen ? <ul onClick={()=>this.handleClick()} className="list-group MobileList">{this.props.options}</ul> : null}</div>);
-  return(<div><Image src={hamburger} onClick= {() => this.handleClick()}  className="MobileMenu"/>
-          {menu} </div>);
-  }
-}
-
-function SidePanel(props) {
-  const history = useHistory();
-  function handleClick(value) {
-    history.push(value);
-  }
-  const sections = [{title:"About", path: "/", hasSubsection: false}, 
-                {title:"Education", path: "/education", hasSubsection: false}, 
-                {title:"Experience", path: "/experience", hasSubsection: false}, 
-                {title:"Projects", path:"/projects", hasSubsection: true}]
-  const sectionDisplaysFull = sections.map((item) => {
-                  return item.hasSubsection ? (
-                    <li className="Section" key={item.title}><NavLink className="links text-decoration-none text-reset" to={item.path}>{item.title}</NavLink></li>
-                      )
-                      : (<li className="Section" key={item.title}><NavLink className="links text-decoration-none text-reset" exact to={item.path}>{item.title}</NavLink></li>
-                      );
-                });
-                //const sectionDisplaysMobile = sections.map(DropdownButton);
-                const sectionDisplaysMobile = sections.map( (item) => {
-                  return item.hasSubsection ? (
-                    <li className="list-group-item" key={item.title}><NavLink className="text-decoration-none text-reset" to={item.path}>{item.title}</NavLink></li>
-                      )
-                      : (<li className="list-group-item" key={item.title}><NavLink className="text-decoration-none text-reset" exact to={item.path}>{item.title}</NavLink></li>
-                      );
-                  });
-                return (
-                  <div className="SidePanel">
-                    <ul className="FullList">{sectionDisplaysFull}</ul>
-                    <div className="MobileMenu"><HamburgerMenu options={sectionDisplaysMobile} /></div>
-                  </div>);
-                  
-
-  }
 
 export default App;
 
